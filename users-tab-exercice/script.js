d2a5ca097085eefe97b8991762fa3ec8532f9d7c
Mustapha Ben <87219978+muben88@users.noreply.github.com>
2022-06-29 21:20:27 +0100

    Add files via upload

---
+++ users-tab-exercice/script.js
@@ -0,0 +1,142 @@
+let arr = [
+  {
+    id: "123456789",
+    createdDate: "2021-01-06T00:00:00.000Z",
+    status: "En validation",
+    firstName: "Mohamed",
+    lastName: "Taha",
+    userName: "mtaha",
+    registrationNumber: "2584",
+  },
+  {
+    id: "987654321",
+    createdDate: "2021-07-25T00:00:00.000Z",
+    status: "Validé",
+    firstName: "Hamid",
+    lastName: "Orrich",
+    userName: "horrich",
+    registrationNumber: "1594",
+  },
+  {
+    id: "852963741",
+    createdDate: "2021-09-15T00:00:00.000Z",
+    status: "Rejeté",
+    firstName: "Rachid",
+    lastName: "Mahidi",
+    userName: "rmahidi",
+    registrationNumber: "3576",
+  },
+];
+
+const usersTable = document.querySelector("#users-table"); // tbody element
+
+const firstName = document.querySelector("#first-name"); // first name input
+const lastName = document.querySelector("#last-name"); // last name input
+const status = document.querySelector("#status"); // status input
+const username = document.querySelector("#username"); // username input
+const createdDate = document.querySelector("#created-date"); // creation date input
+const registrationNumber = document.querySelector("#registration-number"); // registration num input
+const saveUserBtn = document.querySelector("#save-user-btn"); // Save user button
+
+const addUserBtn = document.querySelector("#add-user-btn");
+const dialog = document.querySelector("dialog");
+
+// Show the modal & backdrop
+addUserBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// Hide the modal & remove the backdrop
+dialog.addEventListener("click", (e) => {
+  if (e.target === dialog) {
+    dialog.close();
+  }
+});
+
+// Ids generator
+const uid = () =>
+  Math.floor(Math.random() * Date.now())
+    .toString()
+    .substring(1, 10);
+
+let usersFromLs = localStorage.getItem("users");
+let users = usersFromLs ? JSON.parse(localStorage.getItem("users")) : arr;
+
+// Add a new user to the list
+const addUser = () => {
+  if (
+    firstName.value.length > 0 &&
+    firstName.value.match(/^[A-Za-z]+$/) &&
+    lastName.value.length > 0 &&
+    lastName.value.match(/^[A-Za-z]+$/) &&
+    username.value.length > 0 &&
+    createdDate.value.length >= 10 &&
+    registrationNumber.value.length > 0
+  ) {
+    let user = {
+      id: uid(),
+      createdDate: new Date(createdDate.value).toISOString(),
+      status: status.value,
+      firstName: firstName.value,
+      lastName: lastName.value,
+      userName: username.value,
+      registrationNumber: registrationNumber.value,
+    };
+
+    arr.push(user);
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
+    location.reload(true);
+  }
+};
+
+// Add user on click on save user button
+saveUserBtn.addEventListener("click", () => {
+  addUser();
+});
+
+// Delete a user form the list
+const deleteUser = (index) => {
+  users = JSON.parse(localStorage.getItem("users"));
+  users.splice(index, 1);
+  localStorage.setItem("users", JSON.stringify(users));
+  location.reload(true);
+};
+
+// List the users
+let list = () => {
+  localStorage.setItem("users", JSON.stringify(users));
+  users = JSON.parse(localStorage.getItem("users"));
+  users.map((item) => {
+    let tr = document.createElement("tr");
+    let date = item.createdDate.split("T")[0];
+    let statusClass = "";
+    if (item.status.toLowerCase() == "en validation") {
+      statusClass = "pending";
+    }
+    if (item.status.toLowerCase() == "validé") {
+      statusClass = "validated";
+    }
+    if (item.status.toLowerCase() == "rejeté") {
+      statusClass = "rejected";
+    }
+
+    tr.innerHTML = `<td><span>${item.id}</span></td>
+                    <td><span>${`${date.split("-")[2]}/${date.split("-")[1]}/${
+                      date.split("-")[0]
+                    }`}</span></td>
+                    <td><span class=${statusClass}>${item.status}</span></td>
+                    <td><span>${item.firstName}</span></td>
+                    <td><span>${item.lastName}</span></td>
+                    <td><span>${item.userName}</span></td>
+                    <td><span>${item.registrationNumber}</span></td>
+                    <td><img class="delete_btn" src="assets/delete.png" alt="delete user" onclick="deleteUser(${users.indexOf(
+                      item
+                    )})"/></td>
+        `;
+
+    usersTable.append(tr);
+  });
+};
+
+list();
